8eaa29bcab64bc312c27779a3f0c868d2c97c75c
Tulebaeva Marzhan <241398@astanait.edu.kz>
2025-10-20 16:45:47 +0500

    Log-in

--- WebTechnologiesFinal/assets/js/main.js
+++ WebTechnologiesFinal/assets/js/main.js
@@ -21,3 +21,45 @@ const searchBarIsActive = () => navbarForm.classList.toggle('active');
 navbarSearchBtn.addEventListener('click', searchBarIsActive);
 navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
 
+const signInLink = document.querySelector('.navbar-signin');
+const registerBox = document.getElementById('registerBox');
+const closeBtn = registerBox.querySelector('.close');
+
+signInLink.addEventListener('click', (e) => {
+  e.preventDefault(); 
+  registerBox.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  registerBox.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === registerBox) {
+    registerBox.style.display = 'none';
+  }
+});
+
+  const passwordInput = document.getElementById('password');
+  const message = document.getElementById('passwordMessage');
+
+  passwordInput.addEventListener('input', () => {
+    const value = passwordInput.value;
+
+
+    const hasUppercase = /[A-Z]/.test(value);
+    const hasNumber = /\d/.test(value);
+    const hasLength = value.length >= 8;
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+
+    if (hasUppercase && hasNumber && hasLength && hasSpecialChar) {
+      message.textContent = "Strong password ";
+      message.classList.remove('invalid');
+      message.classList.add('valid');
+    } else {
+      message.textContent = "Password must have: 8+ chars, 1 uppercase, 1 number, 1 symbol ";
+      message.classList.remove('valid');
+      message.classList.add('invalid');
+    }
+  })
+
